f99d040c792a3d17e3c26914ec65061f690b8199
Thomas Shum <jabashque@gmail.com>
2019-04-20 08:48:24 -0700

    First step towards making this my own

--- product.mk
+++ product.mk
@@ -6,14 +6,14 @@ ifeq ($(WITH_GMS), true)
 $(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
 endif
 
-# Signing
-PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/certs/releasekey
-PRODUCT_EXTRA_RECOVERY_KEYS := vendor/certs/releasekey
+## Signing
+#PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/certs/releasekey
+#PRODUCT_EXTRA_RECOVERY_KEYS := vendor/certs/releasekey
 
-# Volume Steps
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.vc_call_vol_steps=7 \
-    ro.config.media_vol_steps=25
+## Volume Steps
+#PRODUCT_PROPERTY_OVERRIDES += \
+    #ro.config.vc_call_vol_steps=7 \
+    #ro.config.media_vol_steps=25
 
 # Recovery ADB keys
 PRODUCT_COPY_FILES += \
